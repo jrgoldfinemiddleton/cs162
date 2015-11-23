0ef858a6d897389b2251a800e0f22de9cc60a4da
Jason Goldfine-Middleton <jrgoldfinemiddleton@gmail.com>
2015-11-23 01:42:11 -0800

    Updated description of assn2 main file

--- assn2/main.cpp
+++ assn2/main.cpp
@@ -1,21 +1,17 @@
 /*********************************************************************
- ** Program Filename:
- ** Author:
- ** Date:
- ** Description:
- ** Input:
- ** Output:
- *********************************************************************/
-
-/*********************************************************************
- ** Function:
- ** Description:
- ** Parameters:
- ** Pre-Conditions:
- ** Post-Conditions:
+ ** Program Filename: main.cpp
+ ** Author: Jason Goldfine-Middleton
+ ** Date: 10/10/15
+ ** Description: This program allows the user to create and modify a
+ **              shopping list with quantity and price information for
+ **              for each item.  The situation is a cross between a
+ **              shopping list, an inventory, and a receipt.
+ ** Input: std::cin, user's menu selections and item information
+ ** Output: std::cout, the list and information about the items on it
  *********************************************************************/
 
 #include "ListClient.hpp"
+
 
 int main()
 {
